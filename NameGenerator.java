39344081845680d9eaf1508a14f38b806684064a
Daniel Pradera <Daniel Pradera>
2016-04-01 12:44:52 +0200

    0104

---
+++ NameGenerator.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class NameGenerator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NameGenerator
+{
+    private String nombreStarWars;
+    
+    /**
+     * Constructor for objects of class NameGenerator
+     */
+    public NameGenerator()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String generateStarWarsName(String nombre, String apellido, String apellidoMadre, String ciudadNac)
+    {
+        nombreStarWars = apellido.substring(0, 3) + nombre.toLowerCase().substring(0, 3) + " " + 
+            apellidoMadre.substring(0, 2) + ciudadNac.toLowerCase().substring(0, 3);
+        return nombreStarWars;
+    }
+}
